4c69dd7cbb133f4798fddb7b7523955f199daaf8
EC2 Default User <ec2-user@ip-172-31-47-5.eu-west-1.compute.internal>
2018-04-27 12:46:54 +0000

    Refactor RangeBuilder specs

--- spec/range_builder_spec.rb
+++ spec/range_builder_spec.rb
@@ -1,11 +1,15 @@
-it 'create_range_of_prices' do
-        range_of_prices = query_strings_builder.create_range_of_prices
+RSpec.describe Fields do
+
+    it 'create_range_of_prices' do
+        range_of_prices = RangeBuilder.create_range_of_prices(start_price = 700, stop_price = 900, interval = 100)
         
         expect(range_of_prices).to eq(["700", "800", "900"])
     end
     
     it 'create_range_of_page_nrs' do
-        range_of_page_nrs = listing.create_range_of_page_nrs
+        range_of_page_nrs = RangeBuilder.create_range_of_page_nrs(max_nr_of_pages = 17)
         
         expect(range_of_page_nrs).to eq(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17"])
     end
+    
+end
